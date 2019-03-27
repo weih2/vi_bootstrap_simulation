a57d07b8bf5d06878c327c124ab73ac6100a0f6f
weih2 <weih2@illinois.edu>
2019-03-27 16:46:22 -0500

    dfa

--- sim_p1/cavi_estimate_weighted_device.cu
+++ sim_p1/cavi_estimate_weighted_device.cu
@@ -60,14 +60,14 @@ void cavi_implementation::device_generate_weights(int exp_id, int thread_id){
   curand_init(exp_id, thread_id, 0, &state);
 
   for(int n_sample = 0; n_sample < *device_n_bootstrap_samples; n_sample++){
-    device_weights[(*device_n_bootstrap_samples) * tread_id + n_sample]
+    device_weights[(*device_n_bootstrap_samples) * thread_id + n_sample]
       = curand_uniform(&state);
   }
 }
 
 __device__  // update estimate per thread per loop
 void cavi_implementation::device_cavi_estimate_weighted(int thread_id){
-  device_elbo[tread_id] = 0;
+  device_elbo[thread_id] = 0;
   double sum_phi;
   int phi_index;
   int par_index;
@@ -75,15 +75,15 @@ void cavi_implementation::device_cavi_estimate_weighted(int thread_id){
 
   for(int i = 0; i < *device_g_vars.device_n_samples; i++){
     sum_phi = 0;
-    weight_index = (*device_n_bootstrap_samples) * tread_id + i;
+    weight_index = (*device_n_bootstrap_samples) * thread_id + i;
 
     for(int k = 0; k < *device_g_vars.device_K; k++){
-      phi_index = tread_id
+      phi_index = thread_id
         * (*device_g_vars.device_n_samples) * (*device_g_vars.device_K)
         + i
         * (*device_g_vars.device_K)
         + k;
-      par_index = tread_id * (*device_g_vars.device_K) + k;
+      par_index = thread_id * (*device_g_vars.device_K) + k;
 
       sum_phi += (
         device_est.device_phi[par_index] =
@@ -93,15 +93,15 @@ void cavi_implementation::device_cavi_estimate_weighted(int thread_id){
       );
     }
     for(int k = 0; k < *device_g_vars.device_K; k++){
-      phi_index = tread_id
+      phi_index = thread_id
         * (*device_g_vars.device_n_samples) * (*device_g_vars.device_K)
         + i
         * (*device_g_vars.device_K)
         + k;
-      par_index = tread_id * (*device_g_vars.device_K) + k;
+      par_index = thread_id * (*device_g_vars.device_K) + k;
 
       device_est.device_phi[phi_index] /= sum_phi;
-      device_elbo[tread_id] -=
+      device_elbo[thread_id] -=
         device_est.device_phi[phi_index]
         * device_weights[weight_index]
         * log(device_est.device_phi[phi_index]);
@@ -113,15 +113,15 @@ void cavi_implementation::device_cavi_estimate_weighted(int thread_id){
     sum_phi = 0;
     product_x_phi = 0;
 
-    par_index = tread_id * (*device_g_vars.device_K) + k;
+    par_index = thread_id * (*device_g_vars.device_K) + k;
 
     for(int i = 0; i < *device_g_vars.device_n_samples; i++){
-      phi_index = tread_id
+      phi_index = thread_id
         * (*device_g_vars.device_n_samples) * (*device_g_vars.device_K)
         + i
         * (*device_g_vars.device_K)
         + k;
-      weight_index = (*device_n_bootstrap_samples) * tread_id + i;
+      weight_index = (*device_n_bootstrap_samples) * thread_id + i;
       sum_phi += device_est.device_phi[phi_index] * device_weights[weight_index];
       product_x_phi += device_x[i] * device_est.device_phi[phi_index]
         * device_weights[weight_index];
@@ -129,19 +129,19 @@ void cavi_implementation::device_cavi_estimate_weighted(int thread_id){
     device_est.device_s2[par_index] = 1 / (1/(*device_g_vars.device_sigma_2) + sum_phi);
     device_est.device_m[par_index] = product_x_phi * device_est.device_s2[par_index];
 
-    device_elbo[tread_id] +=
+    device_elbo[thread_id] +=
       - (product_x_phi * product_x_phi + 1) * device_est.device_s2[par_index]
         /(2*(*device_g_vars.device_sigma_2))
       + log(device_est.device_s2[par_index])/2.;
     for(int i = 0; i < data.g_vars.n_samples; i++){
-      phi_index = tread_id
+      phi_index = thread_id
         * (*device_g_vars.device_n_samples) * (*device_g_vars.device_K)
         + i
         * (*device_g_vars.device_K)
         + k;
-      weight_index = (*device_n_bootstrap_samples) * tread_id + i;
+      weight_index = (*device_n_bootstrap_samples) * thread_id + i;
 
-      device_elbo[tread_id] += device_est.device_phi[phi_index] * device_weights[weight_index]
+      device_elbo[thread_id] += device_est.device_phi[phi_index] * device_weights[weight_index]
         *(device_x[i] * (-device_x[i]/2. + device_est.device_m[par_index])
         - (device_est.device_s2[par_index]
           + device_est.device_m[par_index]*device_est.device_m[par_index])/2.);
@@ -152,18 +152,18 @@ void cavi_implementation::device_cavi_estimate_weighted(int thread_id){
 // for this method we only need vb posterior mean m_k
 __global__
 void cavi_implementation::device_cavi_bootstrap_update_single(){
-  int tread_id = threadIdx.x + blockIdx.x * blockDim.x;
-  if(tread_id >= *device_n_bootstrap_samples) return;
+  int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+  if(thread_id >= *device_n_bootstrap_samples) return;
 
-  const int par_index_start = tread_id * (*device_g_vars.device_K);
-  const int par_index_end = (tread_id + 1) * (*device_g_vars.device_K);
+  const int par_index_start = thread_id * (*device_g_vars.device_K);
+  const int par_index_end = (thread_id + 1) * (*device_g_vars.device_K);
 
-  device_generate_weights((*device_exp_id), tread_id);
+  device_generate_weights((*device_exp_id), thread_id);
   double device_old_elbo;
 
   for(int n_iter = 0; n_iter < *device_max_n_iter; n_iter++){
     device_old_elbo = device_elbo[thread_id];
-    device_cavi_estimate_weighted(tread_id);
+    device_cavi_estimate_weighted(thread_id);
     if(device_elbo[thread_id] - device_old_elbo < *device_epsilon) break;
   }
 
@@ -173,7 +173,7 @@ void cavi_implementation::device_cavi_bootstrap_update_single(){
 
   // rearrange
   for(int par_index = 0; par_index < (*device_g_vars.device_K); par_index++){
-    device_est.device_m_transpose[+ tread_id]
+    device_est.device_m_transpose[+ thread_id]
       = device_est.device_m[par_index_start + par_index * (*device_n_bootstrap_samples)];
   }
 }
