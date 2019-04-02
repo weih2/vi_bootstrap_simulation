eade807662361b1574df8780a3b95f24abc202f8
weih2 <weih2@illinois.edu>
2019-04-02 12:49:33 -0500

    autooooo

--- sim_p1/utils/device_compute.cu
+++ sim_p1/utils/device_compute.cu
@@ -99,7 +99,6 @@ __global__
 void device_cavi_bootstrap_update_single(device_storage device_store){
   int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
   if(thread_id >= *device_store.device_n_bootstrap_samples) return;
-  printf("you still see me..\n");
 
   const int par_index_start = thread_id * (*device_store.device_g_vars.device_K);
   const int par_index_end = (thread_id + 1) * (*device_store.device_g_vars.device_K);
@@ -117,6 +116,8 @@ void device_cavi_bootstrap_update_single(device_storage device_store){
     device_store.device_est.device_m + par_index_end,
     device_store.device_est.device_s2 + par_index_start);
 
+  printf("my id is %d, my estimation is %f\n", 
+    thread_id, *(device_store.device_est.device_m + par_index_end));
   // rearrange
   for(int par_index = 0; par_index < (*device_store.device_g_vars.device_K); par_index++){
     device_store.device_est.device_m_transpose[par_index * (*device_store.device_n_bootstrap_samples) + thread_id]
