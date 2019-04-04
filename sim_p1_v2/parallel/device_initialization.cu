4113d8578d12de890c7ad0cc0969e7e59e7f5851
weih2 <weih2@illinois.edu>
2019-04-03 22:58:44 -0500

    autooooo

--- sim_p1_v2/parallel/device_initialization.cu
+++ sim_p1_v2/parallel/device_initialization.cu
@@ -1,4 +1,5 @@
 __device__ device_cavi_implementation::device_cavi_implementation(device_settings& dev_settings, int t_id){
+  printf("initializing...\n");
   // copy local variables
   for(int k = 0; k < K; k++){
     mu[k] = dev_settings.l_vars.mu[k];
