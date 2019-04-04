64feb3c2b4df81f60e61d5e12c86b48331a69961
weih2 <weih2@illinois.edu>
2019-04-03 23:05:28 -0500

    autooooo

--- sim_p1_v2/cavi_execution.cu
+++ sim_p1_v2/cavi_execution.cu
@@ -1,8 +1,8 @@
-__global__ void cavi_execute(bridge* bg){
+__global__ void cavi_execute(bridge bg){
   int t_id = threadIdx.x + blockIdx.x * blockDim.x;
   if(t_id >= n_experiments) return;
-  printf("%f\n", (bg->device_dev_settings).l_vars.mu[0]);
-  device_cavi_implementation thread_implementation(bg->device_dev_settings, t_id);
+  printf("%f\n", (bg.device_dev_settings).l_vars.mu[0]);
+  device_cavi_implementation thread_implementation(bg.device_dev_settings, t_id);
   printf("my id is %d\n", thread_implementation.thread_id);
   // obtain point estimates
   thread_implementation.device_cavi_point_estimate();
--- sim_p1_v2/connect_to_execution.cu
+++ sim_p1_v2/connect_to_execution.cu
@@ -1,4 +1,4 @@
 void bridge::connect_to_execution(){
-  cavi_execute<<<64,64>>>(this);
+  cavi_execute<<<64,64>>>(*this);
   cudaDeviceSynchronize();
 }
