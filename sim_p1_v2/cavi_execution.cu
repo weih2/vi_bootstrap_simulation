4043bc4cf3d6c813d2219eba23c81b4c0a8e4b84
weih2 <weih2@illinois.edu>
2019-04-03 22:56:56 -0500

    autooooo

--- sim_p1_v2/cavi_execution.cu
+++ sim_p1_v2/cavi_execution.cu
@@ -2,7 +2,7 @@ __global__ void cavi_execute(bridge* bg){
   int t_id = threadIdx.x + blockIdx.x * blockDim.x;
   if(t_id >= n_experiments) return;
   device_cavi_implementation thread_implementation(bg->device_dev_settings, t_id);
-
+  printf("my id is %d\n", thread_implementation.thread_id);
   // obtain point estimates
   thread_implementation.device_cavi_point_estimate();
   for(int k = 0; k < K; k++){
