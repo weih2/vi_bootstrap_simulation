1c3ca5a8eec8e4e1e71f840986cee1f219cc9a8e
weih2 <weih2@illinois.edu>
2019-04-03 23:35:11 -0500

    autooooo

--- sim_p1_v2/parallel/device_initialization.cu
+++ sim_p1_v2/parallel/device_initialization.cu
@@ -31,5 +31,6 @@ __device__ device_cavi_implementation::device_cavi_implementation(device_setting
   for(int i = 0; i < n_samples; i++){
     // let variance be 1
     x[i] = mu[c[i]] + curand_normal_double(&state);
+    if(tread_id == 0) printf("%f\n", mu[c[i]]);
   }
 }
