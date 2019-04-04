e6895614eb104ddc2408eb978cfa594c87dfa73f
weih2 <weih2@illinois.edu>
2019-04-03 21:16:20 -0500

    autooooo

--- sim_p1_v2/cavi_execution.cu
+++ sim_p1_v2/cavi_execution.cu
@@ -1,5 +1,5 @@
 __global__ void cavi_execute(bridge bg){
   int t_id = threadIdx.x + blockIdx.x * blockDim.x;
   if(t_id >= n_experiments) return;
-  device_cavi_implementation thread_implementation(bg.device_settings, t_id);
+  device_cavi_implementation thread_implementation(bg.device_dev_settings, t_id);
 }
