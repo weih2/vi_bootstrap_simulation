256f434c907a34223a1034669b9bdeb5dbbf631b
weih2 <weih2@illinois.edu>
2019-04-03 21:06:32 -0500

    autooooo

--- sim_p1_v2/utils/gen_data.cpp
+++ sim_p1_v2/utils/gen_data.cpp
@@ -1,42 +1,17 @@
 using namespace std;
 
-void generate_latent_pars(simulation_data& sim_data){
-  sim_data.l_vars.mu = new double[sim_data.g_vars.K];
-  sim_data.l_vars.c = new int[sim_data.g_vars.n_samples];
+void generate_latent_pars(local_vars& l_vars){
+  l_vars.mu = new double[g_vars.K];
+  l_vars.c = new int[g_vars.n_samples];
 
   for(int i = 0; i < sim_data.g_vars.K; i++){
-    sim_data.l_vars.mu[i] = sqrt(sim_data.g_vars.sigma_2) * random_normal();
+    l_vars.mu[i] = sqrt(g_vars.sigma_2) * random_normal();
   }
 
-  thrust::sort(sim_data.l_vars.mu, sim_data.l_vars.mu + sim_data.g_vars.K);
+  thrust::sort(l_vars.mu, l_vars.mu + g_vars.K);
 
-  for(int i = 0; i < sim_data.g_vars.n_samples; i++){
-    sim_data.l_vars.c[i] = floor(random_uniform() * sim_data.g_vars.K);
+  for(int i = 0; i < g_vars.n_samples; i++){
+    l_vars.c[i] = floor(random_uniform() * g_vars.K);
   }
-  return;
-}
-
-void generate_data(simulation_data& sim_data){
-  sim_data.x = new double[sim_data.g_vars.n_samples];
-
-  for(int i = 0; i < sim_data.g_vars.n_samples; i++){
-    const int &i_cat = sim_data.l_vars.c[i];
-    sim_data.x[i] = sim_data.l_vars.mu[i_cat] + random_normal();
-  }
-  return;
-}
-
-void generate_weights(simulation_data& sim_data){
-  static int allocated = 0;
-  if(!allocated){
-    sim_data.b_vars.weights = new double[sim_data.g_vars.n_samples];
-    allocated = 1;
-  }
-
-  // generate exponential weigths only
-  for(int i = 0; i < sim_data.g_vars.n_samples ; i++){
-    sim_data.b_vars.weights[i] = - log(random_uniform());
-  }
-
   return;
 }
