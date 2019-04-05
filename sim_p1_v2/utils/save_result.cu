3ba8d217ea28ad5e9f75bca2d36a06ac127fe546
weih2 <weih2@illinois.edu>
2019-04-05 12:14:35 -0500

    autooooo

--- sim_p1_v2/utils/save_result.cu
+++ sim_p1_v2/utils/save_result.cu
@@ -23,15 +23,15 @@ void bridge::save_result(ostream& result_stream){
     // coverate
     result_stream << "number of experiments: " << n_experiments << endl;
 
-    for(int k = 0; k < data.g_vars.K; k++){
+    for(int k = 0; k < K; k++){
       result_stream << "variational wlb confidence set coverage for parameter " << k + 1 << " :" << vwlb_cs_covered_counts[K] << endl;
     }
 
-    for(int k = 0; k < data.g_vars.K; k++){
-      result_stream << "variational confidence set coverage for parameter " << k + 1 << " :" << vp_cs_covered_each[k] << endl;
+    for(int k = 0; k < K; k++){
+      result_stream << "variational confidence set coverage for parameter " << k + 1 << " :" << vp_cs_covered_counts[k] << endl;
     }
 
-    for(int k = 0; k < data.g_vars.K; k++){
+    for(int k = 0; k < K; k++){
       result_stream << "empirical confidence set coverage for parameter " << k + 1 << " :" << empirical_ci_covered_counts[k] << endl;
     }
 
