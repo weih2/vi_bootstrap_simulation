5c801e92e2aa31c9c2de2c5e84059c14d3d11ac7
weih2 <weih2@illinois.edu>
2019-04-04 19:42:20 -0500

    autooooo

--- sim_p1_v2/cavi_execution.cu
+++ sim_p1_v2/cavi_execution.cu
@@ -6,11 +6,13 @@ __global__ void cavi_execute(bridge bg){
   thread_implementation.device_cavi_point_estimate();
 
   thread_implementation.device_weighted_cavi_point_estimate();
+  thread_implementation.device_vwlb_cs_construct();
+  thread_implementation.device_vp_cs_construct();
 
   for(int k = 0; k < K; k++){
     (bg.device_empirical_mu)[t_id + k * n_experiments] = thread_implementation.m[k];
     if(t_id == 0){
-      printf("ci is [%f,%f]", thread_implementation.vwlb_cs[k][0],
+      printf("ci is [%f,%f]\n", thread_implementation.vwlb_cs[k][0],
        thread_implementation.vwlb_cs[k][1]);
     }
   }
