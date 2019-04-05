a1ee72b66af24cf9a645337ba7bddcd6c9d744d7
weih2 <weih2@illinois.edu>
2019-04-04 19:47:44 -0500

    autooooo

--- sim_p1_v2/parallel/device_utils/device_vp_cs_construct.cu
+++ sim_p1_v2/parallel/device_utils/device_vp_cs_construct.cu
@@ -1,4 +1,4 @@
-void device_cavi_implementation::device_vp_cs_construct(){
+__device__ void device_cavi_implementation::device_vp_cs_construct(){
   for(int k = 0; k < K; k++){
     vp_cs[k][0] = -device_ci_quantile * sqrt(s2[k]) + m[k];
     vp_cs[k][1] = device_ci_quantile * sqrt(s2[k]) + m[k];
