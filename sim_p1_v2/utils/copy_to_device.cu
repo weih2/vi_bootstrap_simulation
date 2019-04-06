b8f79c1c74901ff6c93c4ed099da61b1a325e8e8
weih2 <weih2@illinois.edu>
2019-04-06 12:34:20 -0500

    autooooo

--- sim_p1_v2/utils/copy_to_device.cu
+++ sim_p1_v2/utils/copy_to_device.cu
@@ -8,7 +8,6 @@ void bridge::init_device(){
   cudaMalloc((void**)&device_dev_settings.ci_quantile, sizeof(double));
 
   cudaMalloc((void**)&device_dev_settings.data_count, sizeof(int));
-  cudaMemset(device_dev_settings.data_count, 0, sizeof(int));
 
   cudaMalloc((void**)&device_vwlb_cs_covered,
     sizeof(int) * K * n_experiments);
