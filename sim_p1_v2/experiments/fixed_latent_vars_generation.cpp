6b9aa28bcba1307be3fdd9eaf2f8dba1b6dcc583
weih2 <weih2@illinois.edu>
2019-04-06 12:27:36 -0500

    autooooo

--- sim_p1_v2/experiments/fixed_latent_vars_generation.cpp
+++ sim_p1_v2/experiments/fixed_latent_vars_generation.cpp
@@ -1,4 +1,4 @@
-void fixed_latent_vars_generation(latent_vars& l_vars, double& delta){
+void fixed_latent_vars_generation(latent_vars l_vars, double delta){
   l_vars.mu[0] = 0;
   for(int k = 1; k < K; k++){
     l_vars.mu[k] = l_vars.mu[k - 1] + delta;
