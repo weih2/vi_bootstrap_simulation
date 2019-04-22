571aa9fb6ee2c1382caf56d7bae6870b22b44106
weih2 <44448895+weih2@users.noreply.github.com>
2019-04-22 01:37:02 -0500

    smaller sample

--- sim_p1_v2/simulation_settings.cpp
+++ sim_p1_v2/simulation_settings.cpp
@@ -1,6 +1,6 @@
-#define K 3
+#define K 2
 #define sigma_2 10.
-#define n_samples 1000
+#define n_samples 500
 
-#define n_bootstrap_samples 1000
+#define n_bootstrap_samples 500
 #define n_experiments 4096
