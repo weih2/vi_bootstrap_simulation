4e783f72ad8e78b3cb0096622542d73c9ade346e
Wei <weih2@illinois.edu>
2019-07-10 21:27:32 -0500

    fdwg

--- sim_p1_v2/experiments/exp4_result_sigma40.R
+++ sim_p1_v2/experiments/exp4_result_sigma40.R
@@ -4,12 +4,12 @@ result.lab = rep(c("vwlb_cs", "vwlb_cs2", "vp", "empirical"), each = 100)
 result.x.lab = seq(0.1, 10, by = 0.1)
 
 result.df = data.frame(coverage = result.coverage, lab = result.lab, 
-                       x.lab = result.x.lab)
+                       delta = result.x.lab)
 library(ggplot2)
 # color
 ggplot(data = result.df, 
-       mapping = aes(x = x.lab, y = coverage, color = lab)) + geom_line()
+       mapping = aes(x = delta, y = coverage, color = lab)) + geom_line()
 
   # linetype
 ggplot(data = result.df, 
-       mapping = aes(x = x.lab, y = coverage, linetype = lab)) + geom_line()
+       mapping = aes(x = delta, y = coverage, linetype = lab)) + geom_line()
--- sim_p1_v2/simulation_settings.cpp
+++ sim_p1_v2/simulation_settings.cpp
@@ -1,5 +1,5 @@
 #define K 2
-#define sigma_2 20.
+#define sigma_2 25.
 #define n_samples 500
 
 #define n_bootstrap_samples 500
