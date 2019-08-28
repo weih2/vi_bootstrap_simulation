6682f635e2d7b7a5885bc4f260245de56ecbe6d1
root <root@guest.guest>
2019-08-28 00:58:33 +0000

    fewgo

--- sim_3/exp1.R
+++ sim_3/exp1.R
@@ -1,3 +1,6 @@
+library(inline)
+library(RcppArmadillo)
+
 source("gen_data.R")
 source("init_update.R")
 source("gibbs_sampler.R")
@@ -6,18 +9,21 @@ n.burnin.steps = 10000
 
 # calculate the variance
 n.inter = 150
-n.sample = 200
+n.sample = 300
 
 test.seq = floor( exp( seq(5, 8, 0.2)))
 res1 = numeric(0)
 res2 = numeric(0)
 
+res3 = numeric(K)
+res4 = numeric(K)
+
 for(n in test.seq){
   N <<- n
-  var.mu.mean = numeric(K)
+  var.mu.sample = numeric(K)
   var.max.abs.mean = numeric(0)
   
-  for(fdjwo in 1:1000){
+  for(fdjwo in 1:500){
     gen.new.data()
     old.sample = init.update()
     old.sample = gibbs.sampler.cxx(x, old.sample, n.burnin.steps)
@@ -34,14 +40,19 @@ for(n in test.seq){
     var.mu = diag(solve(var.mu))
     var.mu = 1/var.mu
     
-    var.mu.mean = var.mu.mean + var.mu
+    var.mu.sample = rbind(var.mu.sample, var.mu)
   }
   
-  var.mu.mean = var.mu.mean/1000
-  show(var.mu.mean)
+  var.mu.sample = var.mu.sample[-1, ]
+  res3 = rbind(res3, apply(var.mu.sample, 2, mean))
+  res4 = rbind(res4, apply(var.mu.sample, 2, sd))
   res1 = c(res1, mean(var.max.abs.mean))
   res2 = c(res2, sd(var.max.abs.mean))
 }
 
 show(res1)
 show(res2)
+res3 = res3[-1,]
+res4 = res4[-1,]
+show(res3)
+show(res4)
