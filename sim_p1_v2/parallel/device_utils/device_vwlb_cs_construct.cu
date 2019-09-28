fb1a013ef8e09baec574b73c041e074030f299e8
Wei <weih2@illinois.edu>
2019-09-28 12:37:40 -0500

    fewgoeji

--- sim_p1_v2/parallel/device_utils/device_vwlb_cs_construct.cu
+++ sim_p1_v2/parallel/device_utils/device_vwlb_cs_construct.cu
@@ -4,11 +4,13 @@ __device__ void device_cavi_implementation::device_vwlb_cs_construct(){
     vwlb_cs[k][0] = sample_quantile_from_sorted_data(
       map_mu_clean[k], (n_bootstrap_samples - n_outliers) , (1 - device_bootstrap_confidence)/2.
     );
-    vwlb_cs2[k][0] = 2 * m[k] - vwlb_cs[k][0];
+
 
     vwlb_cs[k][1] = sample_quantile_from_sorted_data(
       map_mu_clean[k], (n_bootstrap_samples - n_outliers), (1 + device_bootstrap_confidence)/2.
     );
-    vwlb_cs2[k][1] = 2 * m[k] - vwlb_cs[k][1];
+
+    vwlb_cs2[k][0] = 2 * m[k] - vwlb_cs[k][1];
+    vwlb_cs2[k][1] = 2 * m[k] - vwlb_cs[k][0];
   }
 }
