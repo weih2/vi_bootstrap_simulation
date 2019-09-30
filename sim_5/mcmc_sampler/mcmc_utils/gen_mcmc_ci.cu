d219c2a8c0bc5ea03a5a5b48cfb7a54b99ca4003
Wei <weih2@illinois.edu>
2019-09-30 18:04:29 -0500

    fewg

--- sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_ci.cu
+++ sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_ci.cu
@@ -1,5 +1,5 @@
-__device__ device_mcmc_implementor::construct_mcmc_credible_sets(){
-  for(k = 0; k < N_CENTERS; k++){
+__device__ void device_mcmc_implementor::construct_mcmc_credible_sets(){
+  for(int k = 0; k < N_CENTERS; k++){
     thrust::sort(thrust::device, mu_samples[k], mu_samples[k] + N_MCMC_SAMPLES);
     mcmc_credible_sets[k][0] =
     sample_quantile_from_sorted_data(mu_samples[k], N_MCMC_SAMPLES, 0.025);
