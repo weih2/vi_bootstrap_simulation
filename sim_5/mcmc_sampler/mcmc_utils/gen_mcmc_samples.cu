49291428d3f431e1aa06879a2b5a8b4362a3f2e8
Wei <weih2@illinois.edu>
2019-09-30 15:16:53 -0500

    fewg

--- sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_samples.cu
+++ sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_samples.cu
@@ -2,12 +2,12 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
   curandState state;
   curand_init(thread_id, 2, 0, &state);
 
-  double mu_sample0[K];
-  double cat_prob[K];
+  double mu_sample0[N_CENTERS];
+  double cat_prob[N_CENTERS];
   double cat_prob_normalizer;
 
-  double cat_mu_sum[K];
-  double cat_mu_count[K];
+  double cat_mu_sum[N_CENTERS];
+  double cat_mu_count[N_CENTERS];
 
   double ru;
   double rn;
@@ -16,12 +16,12 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
   int sample_count = 0;
 
   //////// draw a random initial value
-  for(int k = 0; k < K; k++)
+  for(int k = 0; k < N_CENTERS; k++)
     mu_sample0[k] = 0;
 
   //////// burn-in period
   for(int step = 0; step < N_BURN_IN + N_MCMC_SAMPLES * N_INTER; step++){
-    for(int k = 0; k < K; k++){
+    for(int k = 0; k < N_CENTERS; k++){
       cat_mu_sum[k] = 0;
       cat_mu_count[k] = 0;
     }
@@ -29,12 +29,12 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
     for(int i = 0; i < N_OBS; i++){
 
       cat_prob_normalizer = 0;
-      for(int k = 0; k < K; k++){
+      for(int k = 0; k < N_CENTERS; k++){
         cat_prob_normalizer += ( cat_prob[k] = exp(- (obs[i] - mu_sample0[k]))/2. );
       }
       ru = curand_uniform_double(&state);
       ru /= cat_prob_normalizer;
-      for(int k = 0; k < K; k++){
+      for(int k = 0; k < N_CENTERS; k++){
         if(ru < cat_prob[k]){
           cat_mu_count[k]++;
           cat_mu_sum[k] += obs[i];
@@ -45,7 +45,7 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
     }
 
     // sample mu
-    for(int k = 0; k < K; k++){
+    for(int k = 0; k < N_CENTERS; k++){
       sample_var = PRIOR_SIGMA2/(1 + PRIOR_SIGMA2 * cat_mu_count[k]);
       rn = curand_normal_double(&state);
       mu_sample0[k] = rn * sqrt(sample_var) + sample_var * cat_mu_sum[k];
@@ -55,7 +55,7 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
     // take sample if
     if(step >= N_BURN_IN){
       if(step % N_INTER == 0){
-        for(int k = 0; k < K; k++){
+        for(int k = 0; k < N_CENTERS; k++){
           mu_samples[k][sample_count] = mu_sample0[k];
         }
       }
