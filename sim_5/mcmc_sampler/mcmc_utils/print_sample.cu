6875b75a5bfd5eb98a679fdd91058bd0882e66d4
Wei <weih2@illinois.edu>
2019-09-30 18:05:04 -0500

    fewg

--- sim_5/mcmc_sampler/mcmc_utils/print_sample.cu
+++ sim_5/mcmc_sampler/mcmc_utils/print_sample.cu
@@ -1,4 +1,4 @@
-__device__ device_mcmc_implementor::print_sample(int &n_samp){
+__device__ void device_mcmc_implementor::print_sample(int &n_samp){
   printf("sample #%d: ", n_samp);
   for(int k = 0; k < N_CENTERS; k++) printf("%f ", mu_samples[k][n_samp]);
   printf("\n");
