b8813a3512938a4ad9c6e2932fcc40a018ba4a11
Wei <weih2@illinois.edu>
2019-09-30 22:59:01 -0500

    fewg

--- sim_5/mcmc_sampler/connect_execution.cu
+++ sim_5/mcmc_sampler/connect_execution.cu
@@ -6,15 +6,10 @@ __global__ void connect_to_execution
   device_mcmc_implementor thread_implementation(delta, t_id);
   thread_implementation.gen_obs();
   thread_implementation.gen_mcmc_samples();
-  printf("\n");
   thread_implementation.construct_mcmc_credible_sets();
 
-  if(t_id == 1) {
-    printf("\n");
-    thread_implementation.print_sample(N_BURN_IN);
-  }
-
   for(int k = 0; k < N_CENTERS; k++){
+    printf("%d\n", t_id);
     dev_credible_sets_covered[k][t_id] = thread_implementation.covered[k];
     dev_credible_sets_lengths[k][t_id] =
     thread_implementation.mcmc_credible_sets[k][1] - thread_implementation.mcmc_credible_sets[k][0];
