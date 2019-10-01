7e185c8a56be3f81a9122a8642a984f81ce2fabc
Wei <weih2@illinois.edu>
2019-09-30 23:50:40 -0500

    fewg

--- sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_samples.cu
+++ sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_samples.cu
@@ -50,7 +50,9 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
       sample_var = PRIOR_SIGMA2/double(1 + PRIOR_SIGMA2 * cat_mu_count[k]);
       rn = curand_normal_double(&state);
       mu_sample0[k] = rn * sqrt(sample_var) + sample_var * cat_mu_sum[k];
+      if(thread_id == 0) printf("%f ", mu_sample0[k]);
     }
+    printf("\n");
     thrust::sort(thrust::device, mu_sample0, mu_sample0 + N_CENTERS);
     // take sample if
     if(step >= N_BURN_IN){
