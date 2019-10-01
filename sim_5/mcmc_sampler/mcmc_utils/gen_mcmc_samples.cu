7dc99f918b8bd134bffbcc1c655959b7c27993f0
Wei <weih2@illinois.edu>
2019-09-30 23:26:22 -0500

    fewg

--- sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_samples.cu
+++ sim_5/mcmc_sampler/mcmc_utils/gen_mcmc_samples.cu
@@ -42,7 +42,7 @@ __device__ void device_mcmc_implementor::gen_mcmc_samples(){
         ru -= cat_prob[k];
       }
     }
-    if(t_id == 0) for(int k = 0; k < N_CENTERS){
+    if(thread_id == 0) for(int k = 0; k < N_CENTERS){
       printf("%d \n", cat_mu_count[k]);
     }
 
