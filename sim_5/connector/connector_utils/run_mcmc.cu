eb89b0aba86618d7fac01afb5f37e8e3f4868e1b
Wei <weih2@illinois.edu>
2019-09-30 21:57:26 -0500

    fewg

--- sim_5/connector/connector_utils/run_mcmc.cu
+++ sim_5/connector/connector_utils/run_mcmc.cu
@@ -2,4 +2,5 @@ void connector::run_mcmc(double delta){
   connect_to_execution<<<32,32>>>(delta, device_credible_sets_lengths, device_credible_sets_covered);
   cudaDeviceSynchronize();
   connector_copy_back();
+  cudaDeviceSynchronize();
 }
