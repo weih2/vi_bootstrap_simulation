da90d1662bb88cf2470801244238816772c63b11
Wei <weih2@illinois.edu>
2019-09-30 21:50:34 -0500

    fewg

--- sim_5/connector/connector_utils/connector_gen_stats.cu
+++ sim_5/connector/connector_utils/connector_gen_stats.cu
@@ -1,11 +1,11 @@
 void connector::gen_stats(){
   for(int k = 0; k < N_CENTERS; k++){
     // count coverage rates
-    coverage_rates = 0;
+    coverage_rates[k] = 0;
     for(int n = 0; n < N_EXPERIMENTS; n++){
-      coverage_rates += credible_sets_covered[k][n];
+      coverage_rates[k] += credible_sets_covered[k][n];
     }
-    coverage_rates /= double(N_EXPERIMENTS);
+    coverage_rates[k] /= double(N_EXPERIMENTS);
 
     // mean and var stats
     credible_sets_lengths_means[k] = cal_mean(credible_sets_lengths[k], N_EXPERIMENTS);
