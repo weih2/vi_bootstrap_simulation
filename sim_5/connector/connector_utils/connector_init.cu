ecddd14a040ec3f409bfc68f1f3bd7e823bb2681
Wei <weih2@illinois.edu>
2019-10-12 10:54:50 -0500

    wgjo

--- sim_5/connector/connector_utils/connector_init.cu
+++ sim_5/connector/connector_utils/connector_init.cu
@@ -3,11 +3,11 @@ connector::connector(){
     cudaMalloc((void**)& device_credible_sets_lengths, N_CENTERS * N_EXPERIMENTS * sizeof(double));
     cudaMalloc((void**)& device_credible_sets_covered, N_CENTERS * N_EXPERIMENTS * sizeof(int));
   }
-  cudaMalloc((void**)& dev_fi_inv, N_CENTERS * N_CENTERS * N_EXPERIMENTS * sizeof(double));
+  // cudaMalloc((void**)& dev_fi_inv, N_CENTERS * N_CENTERS * N_EXPERIMENTS * sizeof(double));
 }
 
 connector::~connector(){
     cudaFree(device_credible_sets_lengths);
     cudaFree(device_credible_sets_covered);
-    cudaFree(dev_fi_inv);
+    // cudaFree(dev_fi_inv);
 }
