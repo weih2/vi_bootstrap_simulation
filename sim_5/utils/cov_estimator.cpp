286cee142b9560823d71fb9d7c292c6b19b7c871
Wei <weih2@illinois.edu>
2019-10-12 00:34:35 -0500

    fojgw

--- sim_5/utils/cov_estimator.cpp
+++ sim_5/utils/cov_estimator.cpp
@@ -2,7 +2,7 @@ void cov_estimator(double* inv_fi, double* diag_fi){
   LAPACKE_dpotrf(LAPACK_ROW_MAJOR, 'U', N_CENTERS, inv_fi, N_CENTERS);
   LAPACKE_dpotri(LAPACK_ROW_MAJOR, 'U', N_CENTERS, inv_fi, N_CENTERS);
 
-  for(k = 0; k < N_CENTERS; k++){
+  for(int k = 0; k < N_CENTERS; k++){
     diag_fi[k] = inv_fi[k*N_CENTERS + k];
   }
 }
