d7f30568f294c27630d49bc3496339b1de52a008
Wei <weih2@illinois.edu>
2019-10-11 22:06:43 -0500

    fojgw

--- sim_5/connector/connector_utils/invert_fi_back.cu
+++ sim_5/connector/connector_utils/invert_fi_back.cu
@@ -18,7 +18,6 @@ void connector::invert_fi_back(){
 
   for(int n = 0; n < N_EXPERIMENTS; n++)
     device_fi[n] = device_fi_ + N_CENTERS * N_CENTERS * n;
-  printf("so far so good\n");
   /*
   // LU decompositions
   cublasDgetrfBatched(handle, N_CENTERS, dev_fi_inv_,
@@ -31,6 +30,7 @@ void connector::invert_fi_back(){
   cudaMemcpy(fi_est, device_fi_,
     N_CENTERS * N_CENTERS * N_EXPERIMENTS * sizeof(double), cudaMemcpyDeviceToHost);
 
+    printf("so far so good\n");
   cudaFree(p_arr);
   cudaFree(info_arr);
   cudaFree(device_fi_);
