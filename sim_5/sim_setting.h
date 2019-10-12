d708fdef4cf166cd4fb4a438a6da0cf6ce71e14c
Wei <weih2@illinois.edu>
2019-10-12 01:26:07 -0500

    fojgw

--- sim_5/sim_setting.h
+++ sim_5/sim_setting.h
@@ -1,9 +1,9 @@
 #define N_CENTERS 3
 
 #define N_OBS 200
-#define N_MCMC_SAMPLES 8000
+#define N_MCMC_SAMPLES 5000
 
 #define N_EXPERIMENTS 1000
-#define N_INTER 20
+#define N_INTER 50
 
 #define N_BURN_IN 10000
