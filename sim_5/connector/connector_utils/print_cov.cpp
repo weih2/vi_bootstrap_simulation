e7438ddb3b4cbe055b19981726148a359e937f7b
Wei <weih2@illinois.edu>
2019-10-11 23:14:25 -0500

    fojgw

--- sim_5/connector/connector_utils/print_cov.cpp
+++ sim_5/connector/connector_utils/print_cov.cpp
@@ -2,8 +2,8 @@ void connector::print_cov(){
   int cov_count = 0;
   while(cov_count < N_CENTERS * N_CENTERS * N_EXPERIMENTS){
     for(int k = 0; k < N_CENTERS; k++){
-      cov_count++;
       printf("%f ", fi_inv[cov_count]);
+      cov_count++;
     }
     printf("\n");
   }
