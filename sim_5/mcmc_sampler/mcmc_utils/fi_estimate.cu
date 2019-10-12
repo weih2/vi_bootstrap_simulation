7e99e285f1f9d76b5488053e4e95df42a54b65ea
Wei <weih2@illinois.edu>
2019-10-11 21:41:43 -0500

    fojgw

--- sim_5/mcmc_sampler/mcmc_utils/fi_estimate.cu
+++ sim_5/mcmc_sampler/mcmc_utils/fi_estimate.cu
@@ -1,5 +1,5 @@
 __device__ void device_mcmc_implementor::fi_inv_estimate(){
-  
+
   for(int k1 = 0; k1 < N_CENTERS; k1++){
     for(int k2 = 0; k2 <= k1; k2++){
       fi_inv_estimation[k1 * N_CENTERS + k2] = cal_covariance(mu_samples[k1], mu_samples[k2],
@@ -7,4 +7,5 @@ __device__ void device_mcmc_implementor::fi_inv_estimate(){
       fi_inv_estimation[k2 * N_CENTERS + k1] = fi_inv_estimation[k1 * N_CENTERS + k2];
     }
   }
+  if(thread_id == 0) printf("%f\n", fi_inv_estimation[0]);
 }
