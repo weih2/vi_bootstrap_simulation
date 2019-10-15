bdddc2c6ca7372effa4154a560a082b19d7a3ebd
Wei <weih2@illinois.edu>
2019-10-15 00:30:13 -0500

    ewjio

--- sim_p1_v2/experiments/fixed_latent_vars_generation.cpp
+++ sim_p1_v2/experiments/fixed_latent_vars_generation.cpp
@@ -1,6 +1,6 @@
 void fixed_latent_vars_generation(latent_vars l_vars, double delta){
-  l_vars.mu[0] = - floor(K/2) * delta;
-  for(int k = 1; k < K; k++){
+  l_vars.mu[0] = - floor(N_CLUSTERS/2) * delta;
+  for(int k = 1; k < N_CLUSTERS; k++){
     l_vars.mu[k] = l_vars.mu[k-1] + delta;
   }
 }
--- sim_p1_v2/parallel/device_cavi_implementation.h
+++ sim_p1_v2/parallel/device_cavi_implementation.h
@@ -1,6 +1,6 @@
 class device_cavi_implementation{
 public:
-  __device__ device_cavi_implementation(double, int);
+  __device__ device_cavi_implementation(device_settings, int);
   int thread_id;
 
   // double sigma_2;
